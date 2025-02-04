c825e846dc09baefc910e9f313b43c254f943766
Brian Wardwell <brian.wardwell@internal-services-proxy.sre.svc.weedle-us1.staging-fed.dog>
2025-02-03 17:14:38 -0800

    declare vars and types

--- src/main.go
+++ src/main.go
@@ -5,4 +5,12 @@ import "fmt"
 func main() {
 	// #1 - Change the code to print "Starting Textio server...!" instead of "Hello, World!".
 	fmt.Println("Starting Textio server...")
+	// #2 Intial variables/types
+	// initialize variables here
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
+	var username string
+
+	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
