7a53b9d2a35d78299cd80dfa0591d07b1c054f1a
Brian Wardwell <brian.wardwell@internal-services-proxy.sre.svc.weedle-us1.staging-fed.dog>
2025-02-03 16:55:44 -0800

    change text

---
+++ src/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// #1 - Change the code to print "Starting Textio server...!" instead of "Hello, World!".
+	fmt.Println("Starting Textio server...")
+}
