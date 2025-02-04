ca63b994eca6e839eced39d6b7ff45f2513c6431
Brian Wardwell <brian.wardwell@internal-services-proxy.sre.svc.weedle-us1.staging-fed.dog>
2025-02-03 17:31:42 -0800

    Walrus

--- src/main.go
+++ src/main.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 func main() {
+
 	// #1 - Change the code to print "Starting Textio server...!" instead of "Hello, World!".
 	fmt.Println("Starting Textio server...")
-	// #2 Intial variables/types
-	// initialize variables here
+
+	// #2 - Intialize variables/types
 	var smsSendingLimit int
 	var costPerSMS float64
 	var hasPermission bool
 	var username string
 
 	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+
+	// #3 - Assign values to variables and print message
+	messageStart := "Happy birthday! You are now"
+	age := 21
+	messageEnd := "years old!"
+
+	fmt.Println(messageStart, age, messageEnd)
 }
